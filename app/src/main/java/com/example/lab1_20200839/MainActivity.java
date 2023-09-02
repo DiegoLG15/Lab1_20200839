5ffafcd6d3d1355d55c6b406c48f9635d5b6f190
DiegoLG15 <a20200839@pucp.edu.pe>
2023-09-02 02:20:02 -0500

    avance 1 80

--- app/src/main/java/com/example/lab1_20200839/MainActivity.java
+++ app/src/main/java/com/example/lab1_20200839/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.lab1_20200839;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        registerForContextMenu((TextView) findViewById(R.id.textBienvenido));
     }
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo){
+        super.onCreateContextMenu(menu,v,menuInfo);
+        getMenuInflater().inflate(R.menu.menu_context, menu);
+    }
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        // Manejar la selección de color del menú contextual
+        switch (item.getItemId()) {
+            case R.id.menu_blue:
+                title.setTextColor(getResources().getColor(R.color.blue));
+                return true;
+            case R.id.menu_green:
+                titleTextView.setTextColor(getResources().getColor(R.color.green));
+                return true;
+            case R.id.menu_red:
+                titleTextView.setTextColor(getResources().getColor(R.color.red));
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
 }
